c1fb6a8c3165ee4373c88fa8a4ca4b2141fb0902
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-02-26 11:20:59 -0300

    new routes

--- Medical manager/src/controllers/physicianController.js
+++ Medical manager/src/controllers/physicianController.js
@@ -1,6 +1,7 @@
 // newPhysician, updatePhysician, deletePhysician;
 
 const Physician = require('../models/Physician');
+// const Appointment = require('../models/Appointment');
 
 module.exports = {
   async listAllPhysicians(req, res, next) {
@@ -12,5 +13,42 @@ module.exports = {
     return physicians
       ? res.status(200).json({ physicians })
       : res.status(404).json({ msg: 'Não foi possível encontrar médicos.' });
+  },
+
+  async newPhysician(req, res, next) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) return res.status(404).json({ msg: 'Dados obrigatórios não foram preenchidos.' });
+
+    const physicianExists = await Physician.findOne({ where: { email } });
+
+    if (physicianExists) return res.status(403).json({ msg: 'Médico já cadastrado.' });
+    else {
+      const physician = await Physician.create({
+        name,
+        email,
+        password,
+      }).catch((err) => {
+        res.status(500).json({ msg: 'Falha na conexão.' });
+      });
+
+      physician
+        ? res.status(201).json({ msg: 'Médico criado com sucesso.' })
+        : res.status(400).json({ msg: 'Não foi possível cadastrar novo médico.' });
+    }
+  },
+
+  async deletePhysician(req, res, next) {
+    const physicianId = req.params?.id;
+
+    // const appointmentExists = await Appointment.findOne({ where: { physicianId } });
+    // if (appointmentExists) return res.status(403).json({ msg: 'Médico possui consultas.' });
+
+    const deletedPhysician = await Physician.destroy({
+      where: { id: physicianId },
+    });
+
+    return deletedPhysician
+      ? res.status(200).json({ msg: 'Médico excluído com sucesso.' })
+      : res.status(404).json({ msg: 'Médico não encontrado.' });
   },
 };
--- Medical manager/src/routes/physicianRouter.js
+++ Medical manager/src/routes/physicianRouter.js
@@ -3,5 +3,7 @@ const physicianRouter = express.Router();
 const physicianController = require('../controllers/physicianController');
 
 physicianRouter.get('/listAllPhysicians', physicianController.listAllPhysicians);
+physicianRouter.post('/newPhysician', physicianController.newPhysician);
+physicianRouter.delete('/deletePhysician/:id', physicianController.deletePhysician);
 
 module.exports = physicianRouter;
