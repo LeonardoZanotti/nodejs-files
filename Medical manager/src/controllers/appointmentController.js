7bc658c6e31190a91e1eeed31b7c3b7104b3b07f
Fernando Carvalho Birznek <fernandocbirznek@hotmail.com>
2022-03-03 20:06:40 -0300

    pequenas alterações return, verificação de campo vazio ""

--- Medical manager/src/controllers/appointmentController.js
+++ Medical manager/src/controllers/appointmentController.js
@@ -9,26 +9,26 @@ module.exports = {
     const appointmentDate = new Date(req.body?.appointmentDate);
     console.log(appointmentDate);
     if (!physicianId || !patientId || !description || !appointmentDate) {
-      res.status(400).json({ msg: 'Dados obrigatórios não foram preenchidos.' });
-    }
-
-    const appointmentExists = await Appointment.findOne({
-      where: { physicianId, appointmentDate },
-    });
-
-    if (appointmentExists) res.status(403).json({ msg: 'Já há uma consulta para este horário.' });
-    else {
-      const appointment = await Appointment.create({
-        physicianId,
-        patientId,
-        description,
-        appointmentDate,
-      }).catch((error) => {
-        res.status(500).json({ msg: 'Não foi possível inserir os dados.' });
+      res.status(400).json({ msg: 'Dados obrigatórios não foram preenchidos ou .' });
+    } else {
+      const appointmentExists = await Appointment.findOne({
+        where: { physicianId, appointmentDate },
       });
-      return appointment
-        ? res.status(201).json({ msg: 'Consulta marcada.' })
-        : res.status(400).json({ msg: 'Não foi possível agendar a consulta.' });
+  
+      if (appointmentExists) res.status(403).json({ msg: 'Já há uma consulta para este horário.' });
+      else {
+        const appointment = await Appointment.create({
+          physicianId,
+          patientId,
+          description,
+          appointmentDate,
+        }).catch((error) => {
+          res.status(500).json({ msg: 'Não foi possível inserir os dados.' });
+        });
+        return appointment
+          ? res.status(201).json({ msg: 'Consulta marcada.' })
+          : res.status(400).json({ msg: 'Não foi possível agendar a consulta.' });
+      }
     }
   },
 
@@ -38,7 +38,7 @@ module.exports = {
     const appointments = await Appointment.findAll({
       where: { patientId },
     });
-    return appointments
+    return appointments && appointments != ""
       ? res.status(200).json({ appointments })
       : res.status(404).json({ msg: 'Paciente não encontrado.' });
   },
@@ -49,7 +49,7 @@ module.exports = {
     const appointments = await Appointment.findAll({
       where: { physicianId },
     });
-    return appointments
+    return appointments && appointments != ""
       ? res.status(200).json({ appointments })
       : res.status(404).json({ msg: 'Médico não encontrado.' });
   },
@@ -61,7 +61,7 @@ module.exports = {
       where: { id: appointmentId },
     });
 
-    return deletedPhysician
+    return deletedPhysician && deletedPhysician != ""
       ? res.status(200).json({ msg: 'Consulta excluída com sucesso.' })
       : res.status(404).json({ msg: 'Consulta não encontrada.' });
   },
